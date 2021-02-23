f24b1fd26ae9480731217cfb7a6392e9065cb349
Asher <grit.developer.asher@gmail.com>
2021-02-23 17:30:46 +0900

    Add module sort products

--- dist/js/common.js
+++ dist/js/common.js
@@ -1,7 +1,57 @@
 'use strict';
 
-function init(){
+function sortShape() {
+    const productLists = document.querySelector('.products_list'),
+        products = document.querySelectorAll('.products'),
+        sortTypeName = this.getAttribute('data-shape');
 
+    products.forEach((product) => {
+        const productType = product.querySelector('img').getAttribute('data-shape');
+
+        if(productType === sortTypeName) {
+            productLists.appendChild(product);
+            product.classList.remove('off');
+        } else {
+            product.classList.add('off');
+        }
+    });
+}
+
+function sortColor() {
+    const productLists = document.querySelector('.products_list'),
+        products = document.querySelectorAll('.products'),
+        sortTypeName = this.getAttribute('data-color');
+
+    products.forEach((product) => {
+        const productType = product.querySelector('img').getAttribute('data-color');
+
+        if(productType === sortTypeName) {
+            productLists.appendChild(product);
+            product.classList.remove('off');
+        } else {
+            product.classList.add('off');
+        }
+    });
+}
+
+function clickSortButton(sortType, cate) {
+    const btnSort = document.querySelector(`${sortType}`);
+    
+    if(cate === 'shape') {
+        btnSort.addEventListener('click', sortShape);
+    } else if(cate === 'color'){
+        btnSort.addEventListener('click', sortColor);
+    }
+    
+}
+
+function init(){
+    clickSortButton('.sort_shirt', 'shape');
+    clickSortButton('.sort_pants', 'shape');
+    clickSortButton('.sort_skirt', 'shape');
+    clickSortButton('.sort_blue', 'color');
+    clickSortButton('.sort_yellow', 'color');
+    clickSortButton('.sort_pink', 'color');
 };
 
 init();
